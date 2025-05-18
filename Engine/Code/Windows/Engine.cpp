031396c68ae1751feef704c7ab0d60c86c6bcf6c
Gustavo Goedert <gusgoe@gmail.com>
2025-05-18 17:21:11 -0400

    add DirectXTK12 support add basic directx style game loop and resource management add .editorconfig code style config

--- Engine/Code/Windows/Engine.cpp
+++ Engine/Code/Windows/Engine.cpp
@@ -1,9 +1,315 @@
-// Engine.cpp : Defines the functions for the static library.
+//
+// Game.cpp
 //
 
+#include "Framework.h"
 #include "lEngine.h"
+#include "Engine.h"
 
-// TODO: This is an example of a library function
-void fnEngine()
+extern void ExitGame() noexcept;
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+using Microsoft::WRL::ComPtr;
+
+Game::Game() noexcept(false)
 {
+    m_deviceResources = std::make_unique<DX::DeviceResources>();
+    m_deviceResources->RegisterDeviceNotify(this);
 }
+
+Game::~Game()
+{
+    if (m_deviceResources)
+    {
+        m_deviceResources->WaitForGpu();
+    }
+}
+
+// Initialize the Direct3D resources required to run.
+void Game::Initialize(HWND window, int width, int height)
+{
+    m_deviceResources->SetWindow(window, width, height);
+
+    m_deviceResources->CreateDeviceResources();
+    CreateDeviceDependentResources();
+
+    m_deviceResources->CreateWindowSizeDependentResources();
+    CreateWindowSizeDependentResources();
+
+    // TODO: Change the timer settings if you want something other than the default variable timestep mode.
+    // e.g. for 60 FPS fixed timestep update logic, call:
+    /*
+    m_timer.SetFixedTimeStep(true);
+    m_timer.SetTargetElapsedSeconds(1.0 / 60);
+    */
+}
+
+#pragma region Frame Update
+// Executes the basic game loop.
+void Game::Tick()
+{
+    m_timer.Tick([&]()
+    {
+        Update(m_timer);
+    });
+
+    Render();
+}
+
+// Updates the world.
+void Game::Update(DX::StepTimer const& timer)
+{
+    PIXBeginEvent(PIX_COLOR_DEFAULT, L"Update");
+
+    auto time = static_cast<float>(timer.GetTotalSeconds());
+
+    // TODO: Add your game logic here.
+#if 0
+    m_world = Matrix::CreateRotationZ(cosf(time) * 2.f);
+#endif
+
+#if 1
+    m_world = Matrix::CreateRotationY(time);
+#endif
+
+    PIXEndEvent();
+}
+#pragma endregion
+
+#pragma region Frame Render
+// Draws the scene.
+void Game::Render()
+{
+    // Don't try to render anything before the first Update.
+    if (m_timer.GetFrameCount() == 0)
+    {
+        return;
+    }
+
+    // Prepare the command list to render a new frame.
+    m_deviceResources->Prepare();
+    Clear();
+
+    auto commandList = m_deviceResources->GetCommandList();
+    PIXBeginEvent(commandList, PIX_COLOR_DEFAULT, L"Render");
+
+    // TODO: Add your rendering code here.
+    ID3D12DescriptorHeap* heaps[] = { m_resourceDescriptors->Heap(), m_states->Heap() };
+    commandList->SetDescriptorHeaps(static_cast<UINT>(std::size(heaps)), heaps);
+
+    m_effect->SetWorld(m_world);
+
+    m_effect->Apply(commandList);
+
+    m_shape->Draw(commandList);
+
+    PIXEndEvent(commandList);
+
+    // Show the new frame.
+    PIXBeginEvent(PIX_COLOR_DEFAULT, L"Present");
+    m_deviceResources->Present();
+    m_graphicsMemory->Commit(m_deviceResources->GetCommandQueue());
+    PIXEndEvent();
+}
+
+// Helper method to clear the back buffers.
+void Game::Clear()
+{
+    auto commandList = m_deviceResources->GetCommandList();
+    PIXBeginEvent(commandList, PIX_COLOR_DEFAULT, L"Clear");
+
+    // Clear the views.
+    auto rtvDescriptor = m_deviceResources->GetRenderTargetView();
+    auto dsvDescriptor = m_deviceResources->GetDepthStencilView();
+
+    commandList->OMSetRenderTargets(1, &rtvDescriptor, FALSE, &dsvDescriptor);
+    commandList->ClearRenderTargetView(rtvDescriptor, Colors::CornflowerBlue, 0, nullptr);
+    commandList->ClearDepthStencilView(dsvDescriptor, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
+
+    // Set the viewport and scissor rect.
+    auto viewport = m_deviceResources->GetScreenViewport();
+    auto scissorRect = m_deviceResources->GetScissorRect();
+    commandList->RSSetViewports(1, &viewport);
+    commandList->RSSetScissorRects(1, &scissorRect);
+
+    PIXEndEvent(commandList);
+}
+#pragma endregion
+
+#pragma region Message Handlers
+// Message handlers
+void Game::OnActivated()
+{
+    // TODO: Game is becoming active window.
+}
+
+void Game::OnDeactivated()
+{
+    // TODO: Game is becoming background window.
+}
+
+void Game::OnSuspending()
+{
+    // TODO: Game is being power-suspended (or minimized).
+}
+
+void Game::OnResuming()
+{
+    m_timer.ResetElapsedTime();
+
+    // TODO: Game is being power-resumed (or returning from minimize).
+}
+
+void Game::OnWindowMoved()
+{
+    auto r = m_deviceResources->GetOutputSize();
+    m_deviceResources->WindowSizeChanged(r.right, r.bottom);
+}
+
+void Game::OnWindowSizeChanged(int width, int height)
+{
+    if (!m_deviceResources->WindowSizeChanged(width, height))
+        return;
+
+    CreateWindowSizeDependentResources();
+
+    // TODO: Game window is being resized.
+}
+
+// Properties
+void Game::GetDefaultSize(int& width, int& height) const noexcept
+{
+    // TODO: Change to desired default window size (note minimum size is 320x200).
+    width = 1920;
+    height = 1080;
+}
+#pragma endregion
+
+#pragma region Direct3D Resources
+// These are the resources that depend on the device.
+void Game::CreateDeviceDependentResources()
+{
+    auto device = m_deviceResources->GetD3DDevice();
+
+    // Check Shader Model 6 support
+    D3D12_FEATURE_DATA_SHADER_MODEL shaderModel = { D3D_SHADER_MODEL_6_0 };
+    if (FAILED(device->CheckFeatureSupport(D3D12_FEATURE_SHADER_MODEL, &shaderModel, sizeof(shaderModel)))
+        || (shaderModel.HighestShaderModel < D3D_SHADER_MODEL_6_0))
+    {
+#ifdef _DEBUG
+        OutputDebugStringA("ERROR: Shader Model 6.0 is not supported!\n");
+#endif
+        throw std::runtime_error("Shader Model 6.0 is not supported!");
+    }
+
+    // TODO: Initialize device dependent objects here (independent of window size).
+    m_graphicsMemory = std::make_unique<GraphicsMemory>(device);
+
+    m_states = std::make_unique<CommonStates>(device);
+
+    m_resourceDescriptors = std::make_unique<DescriptorHeap>(device,
+        Descriptors::Count);
+
+    m_shape = GeometricPrimitive::CreateSphere();
+
+#if 0
+    m_shape = GeometricPrimitive::CreateTorus();
+#endif
+
+    ResourceUploadBatch resourceUpload(device);
+
+    resourceUpload.Begin();
+
+    DX::ThrowIfFailed(
+        CreateWICTextureFromFile(device, resourceUpload, L"earth.bmp",
+            m_texture.ReleaseAndGetAddressOf(), false));
+
+    CreateShaderResourceView(device, m_texture.Get(),
+        m_resourceDescriptors->GetCpuHandle(Descriptors::Earth));
+
+#if 1
+    m_shape->LoadStaticBuffers(device, resourceUpload);
+#endif
+
+    auto uploadResourcesFinished = resourceUpload.End(
+        m_deviceResources->GetCommandQueue());
+
+    uploadResourcesFinished.wait();
+
+    RenderTargetState rtState(m_deviceResources->GetBackBufferFormat(),
+        m_deviceResources->GetDepthBufferFormat());
+
+    EffectPipelineStateDescription pd(
+        &GeometricPrimitive::VertexType::InputLayout,
+        CommonStates::Opaque,
+        CommonStates::DepthDefault,
+        CommonStates::CullNone,
+        rtState);
+
+    //m_effect = std::make_unique<BasicEffect>(device, EffectFlags::Lighting, pd);
+    //m_effect = std::make_unique<BasicEffect>(device, EffectFlags::Lighting | EffectFlags::Texture, pd);
+    m_effect = std::make_unique<BasicEffect>(device, EffectFlags::PerPixelLighting | EffectFlags::Texture, pd);
+    //m_effect->EnableDefaultLighting();
+    m_effect->SetLightEnabled(0, true);
+    m_effect->SetLightDiffuseColor(0, Colors::White);
+    m_effect->SetLightDirection(0, -Vector3::UnitZ);
+
+    m_effect->SetTexture(m_resourceDescriptors->GetGpuHandle(Descriptors::Earth),
+        m_states->AnisotropicWrap());
+
+    m_world = Matrix::Identity;
+}
+
+// Allocate all memory resources that change on a window SizeChanged event.
+void Game::CreateWindowSizeDependentResources()
+{
+    // TODO: Initialize windows-size dependent objects here.
+    auto size = m_deviceResources->GetOutputSize();
+
+    m_view = Matrix::CreateLookAt(Vector3(2.f, 2.f, 2.f),
+        Vector3::Zero, Vector3::UnitY);
+    m_proj = Matrix::CreatePerspectiveFieldOfView(XM_PI / 4.f,
+        float(size.right) / float(size.bottom), 0.1f, 10.f);
+
+    m_effect->SetView(m_view);
+    m_effect->SetProjection(m_proj);
+}
+
+void Game::OnDeviceLost()
+{
+    // TODO: Add Direct3D resource cleanup here.
+    m_shape.reset();
+    m_effect.reset();
+    m_texture.Reset();
+    m_resourceDescriptors.reset();
+    m_states.reset();
+    m_graphicsMemory.reset();
+}
+
+void Game::OnDeviceRestored()
+{
+    CreateDeviceDependentResources();
+
+    CreateWindowSizeDependentResources();
+}
+#pragma endregion
+
+Lumen::Engine::Engine() {}
+Lumen::Engine::~Engine() {}
+
+void Lumen::Engine::Initialize(HWND window, int width, int height) {}
+
+void Lumen::Engine::Tick() {}
+
+void Lumen::Engine::OnActivated() {}
+void Lumen::Engine::OnDeactivated() {}
+void Lumen::Engine::OnSuspending() {}
+void Lumen::Engine::OnResuming() {}
+void Lumen::Engine::OnWindowMoved() {}
+void Lumen::Engine::OnWindowSizeChanged(int width, int height) {}
+
+void Lumen::Engine::GetDefaultSize(int &width, int &height) const noexcept {}
+
+Lumen::Engine *Lumen::GetEngine() { return new Lumen::Engine(); }
--- Engine/Include/Windows/lEngine.h
+++ Engine/Include/Windows/lEngine.h
@@ -1,1 +1,43 @@
-void fnEngine();
+//
+// Engine.h
+//
+
+#pragma once
+
+/// Lumen namespace
+namespace Lumen
+{
+
+    class Engine
+    {
+    public:
+
+        Engine() noexcept(false);
+        ~Engine();
+
+        Engine(Engine &&) = default;
+        Engine &operator= (Engine &&) = default;
+
+        Engine(Engine const &) = delete;
+        Engine &operator= (Engine const &) = delete;
+
+        // Initialization and management
+        void Initialize(HWND window, int width, int height);
+
+        // Basic engine loop
+        void Tick();
+
+        // Messages
+        void OnActivated();
+        void OnDeactivated();
+        void OnSuspending();
+        void OnResuming();
+        void OnWindowMoved();
+        void OnWindowSizeChanged(int width, int height);
+
+        // Properties
+        void GetDefaultSize(int &width, int &height) const noexcept;
+    };
+
+    Engine *GetEngine();
+}
